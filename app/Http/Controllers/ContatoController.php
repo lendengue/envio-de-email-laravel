ee5a59954296eaa4c40dd9c729ab347ed3bb7eaf
ilcavalcante <ilcavalcante@sfiec.org.br>
2022-06-21 15:55:21 -0300

    update atualizacao controller

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-// use App\Mail\ContatoMail;
-// use Mail;
+use App\Mail\ContatoMail;
+use Mail;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class ContatoController extends Controller
 {
@@ -16,18 +17,18 @@ class ContatoController extends Controller
 
     public function sendContato(Request $r){     
         $dados = $r;
-
-        try {
+       
+        // try {
             // Email para o solicitante
-            $view = View::make('emails.contato-email', ['dados' => $dados])->render();  
-            $esg_mail_cliente = new EsgMail($view);
-            $esg_mail_cliente->subject('Nova mensagem recebida');
-            $esg_mail_cliente->from('email@gmail.com','Laravel');
-            Mail::to($dados->email, $dados->nome)->send($esg_mail_cliente); 
+            $view = View::make('emails.contato-email', ['dados' => $dados])->render();
+            $contato_mail = new ContatoMail($view);
+            $contato_mail->subject('Nova mensagem recebida');
+            $contato_mail->from('email@gmail.com','Laravel');
+            Mail::to($dados->email, $dados->nome)->send($contato_mail); 
 
-        } catch (\Throwable $th) {
+        // } catch (\Throwable $th) {
             return redirect()->route('contato-view')->with('error', 'Ocorreu um erro!');
-        }
+        // }
         return redirect()->route('contato-view')->with('success', 'Mensagem enviada!');
     }
 }
